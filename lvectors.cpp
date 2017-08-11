77f39c8062ca4590a08f5becee1e02d209e3d972
4lhc <444lhc@gmail.com>
2017-08-12 03:15:58 +0530

    Operator overloads

--- lvectors.cpp
+++ lvectors.cpp
@@ -16,46 +16,54 @@ int main()
 {
 
     //zero vector x of size 5
-    LaffVector x(5);
-    //Populate with constant values
-    x.Constant(1.01);
+    LaffVector x{5};
     //populate with random numbers in the range 0 to 10: to 3 decimal points
-    x.Random(0, 10, 3);
-    //SCAL: scale x with 0.5
-    x.LaffScale(0.5);
-    //print x
-    x.Display();
+    x.Randomize(0, 10, 3);
+    std::cout << "x: " << x;
 
-    LaffVector xc(5);
+    //Populate with constant values
+    LaffVector y{5, 1.1};
+    //SCAL: scale x with 0.5
+    std::cout << "2 * y: " << y;
+
+    LaffVector xc{5};
     //COPY
     xc = xc.LaffCopy(x);
-    xc.Display();
+    std::cout << "xc " << xc;
 
+    //Define a LaffVector z with a vector
     std::vector<double> v{1.2, 2.3, 4.21, 1.3, 1.0};
-    //Define a LaffVector y with v
-    LaffVector y(v);
-    y.Display();
+    LaffVector z{v};
+    std::cout << "z: " << z;
+
+    //ADD
+    //
+    std::cout << " x + y: " << (x + y);
+
 
     //AXPY
-    y = y.LaffAxpy(0.5, x);
-    y.Display();
+    //z = 1.5*x + y
+    std::cout << "AXPY: 1.5x + y: " << ((1.5*x ) + y);
+
+    //Linear combination of vectors
+    std::cout << "x + 2y + 3z: " << (x + (2*y) + (3*z));
 
     //DOT & NORM2
-    std::cout << "dot(x, y) = " << LaffVector::LaffDot(x, y) << std::endl;
-    std::cout << "length(x) = " << LaffVector::LaffNorm2(x) << std::endl;
-    std::cout << "length(y) = " << LaffVector::LaffNorm2(y) << std::endl;
+    std::cout << "dot(x, y) = " << Dot(x, y) << std::endl;
+    std::cout << "length(x) = " << Norm2(x) << std::endl;
+    std::cout << "length(y) = " << Norm2(y) << std::endl;
 
-    LaffVector z(10);
-    z.Random(0, 50);
-    z.Display();
+    LaffVector r(10);
+    r.Randomize(0, 50);
+    std::cout << "r: " << r;
 
 
     //SLICE
-    LaffVector forward_slice = z.LaffSlice(2, 5, 1);
-    LaffVector reverse_slice = z.LaffSlice(8, 3, -2); //with stride -2
+    LaffVector forward_slice = r.Slice(2, 5, 1);
+    LaffVector reverse_slice = r.Slice(8, 3, -2); //with stride -2
 
-    forward_slice.Display();
-    reverse_slice.Display();
+    std::cout << "Slice(2, 5, 1): " << forward_slice;
+    std::cout << "Slice(8, 3, -2): " << reverse_slice;
 
     return 0;
 }
