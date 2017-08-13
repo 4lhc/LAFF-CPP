a42be891a197c70570ce9bba5bfc76bfa5a1770a
4lhc <444lhc@gmail.com>
2017-08-13 12:17:13 +0530

    implementation file

--- lvectors.cpp
+++ lvectors.cpp
@@ -12,58 +12,203 @@
 
 #include "lvectors.h"
 
-int main()
+LaffVector::LaffVector()
+    : _lvector(0)
+{}
+LaffVector::LaffVector(int size)
+    : _lvector(size, 0.0)
+{}
+
+LaffVector::LaffVector(int size, double elements)
+    : _lvector(size, elements)
+{}
+
+LaffVector::LaffVector(std::vector<double> v)
+    : _lvector(v)
+{}
+
+LaffVector::LaffVector(std::vector<double> v, std::string &vname)
+    : _lvector(v), _name(vname)
+{}
+
+LaffVector& LaffVector::Copy(LaffVector &v)
+{
+    //copy vector
+    //y:=x
+    //
+    //check if they are equal length
+    if (this->Size() != v.Size())
+    {
+
+        throw std::invalid_argument("Size mismatch");
+    }
+
+    for(int i = 0; i<v.Size(); i++)
+    {
+        this->_lvector[i] = v._lvector[i];
+    }
+
+    return *this;
+}
+
+
+
+
+
+LaffVector LaffVector::Slice(int start, int finish, int inc)
+{
+    //Slice a vector
+    //
+    //
+
+    LaffVector sliced_vector(0);
+    //LaffVector sliced_vector(abs(finish-start)+1);
+    if (inc >= 0 && finish >= start)
+    {
+        const_iter it_begin = _lvector.begin() + start;
+        const_iter it_end = _lvector.begin() + finish + 1;
+        auto it = it_begin;
+        while (it != it_end)
+        {
+            sliced_vector._lvector.push_back(*it);
+            it += inc;
+        }
+    }
+    else if (inc < 0 && finish <= start)
+    {
+        const_iter it_begin = _lvector.begin() + start;
+        const_iter it_end = _lvector.begin() + finish - 1;
+        auto it = it_begin;
+        while (it != it_end)
+        {
+            sliced_vector._lvector.push_back(*it);
+            it -= abs(inc);
+
+        }
+    }
+    else
+    {
+        throw std::invalid_argument("Illegal range");
+    }
+    return sliced_vector;
+}
+
+
+void LaffVector::Randomize(int l_range, int u_range, int digits )
+{
+    //populate with random elements
+    //
+    //
+    srand(time(NULL)); //init seed
+    auto it = _lvector.begin();
+    auto it_end = _lvector.end();
+    int mf = pow(10, digits); //decimal points mult_factor
+    while(it != it_end)
+    {
+
+        *it =  static_cast<double>(rand() % (u_range * mf) + l_range * mf)/mf;
+        it++;
+    }
+
+}
+
+LaffVector operator*(const double &alpha, const LaffVector &v)
+{
+    //scale vector
+    //x:=ax
+    //
+    LaffVector scaled_v = v;
+    auto it = scaled_v._lvector.begin();
+    auto it_end = scaled_v._lvector.end();
+    while(it != it_end)
+    {
+        *it *= alpha;
+        it++;
+    }
+    return scaled_v;
+
+}
+
+LaffVector operator+(const LaffVector &v1, const LaffVector &v2)
+{
+    //x+y
+    //
+    //checking for equal length
+    LaffVector added_v= v1;
+    if (v2.Size() != v1.Size())
+    {
+        throw std::invalid_argument("Size mismatch");
+    }
+
+    for(unsigned int i = 0; i<v1._lvector.size(); i++)
+    {
+        added_v._lvector[i] = v1._lvector[i] + v2._lvector[i];
+    }
+
+
+    return added_v;
+}
+
+
+double Dot(const LaffVector &v1, const LaffVector &v2)
+{
+    //Dot product
+    //a:=x'y
+    //
+    //checking for equal length
+    if (v1.Size() != v2.Size())
+    {
+        throw std::invalid_argument("Size mismatch");
+    }
+    double aplha{0};
+
+    for(int i = 0; i<v1.Size(); i++)
+    {
+        aplha += v1._lvector[i] * v2._lvector[i];
+    }
+    return aplha;
+}
+
+
+
+double Norm2(const LaffVector &v)
+{
+    //Length of vector
+    //a:=||x||2
+    //
+    return sqrt(Dot(v, v));
+}
+
+
+bool Equal(const LaffVector &v1, const LaffVector &v2)
+{
+    //check if two vectors are equal
+    bool are_equal = false;
+    if (v1.Size() == v2.Size())
+    {
+        are_equal = true;
+        for (int i = 0; i < v1.Size(); i++)
+        {
+            if (v1._lvector[i] != v2._lvector[i])
+            {
+                are_equal = false;
+                break;
+            }
+        }
+    }
+    return are_equal;
+}
+
+
+std::ostream &operator<<(std::ostream &output, const LaffVector &v)
 {
 
-    //zero vector x of size 5
-    LaffVector x{5};
-    //populate with random numbers in the range 0 to 10: to 3 decimal points
-    x.Randomize(0, 10, 3);
-    std::cout << "x: " << x;
-
-    //Populate with constant values
-    LaffVector y{5, 1.1};
-    //SCAL: scale x with 0.5
-    std::cout << "2 * y: " << y;
-
-    LaffVector xc{5};
-    //COPY
-    xc = xc.Copy(x);
-    std::cout << "xc " << xc;
-
-    //Define a LaffVector z with a vector
-    //std::vector<double> v{1.2, 2.3, 4.21, 1.3, 1.0};
-    LaffVector z{{1.2, 2.3, 4.21, 1.3, 1.0}};
-    std::cout << "z: " << z;
-
-    //ADD
-    //
-    std::cout << " x + y: " << (x + y);
-
-
-    //AXPY
-    //z = 1.5*x + y
-    std::cout << "AXPY: 1.5x + y: " << ((1.5*x ) + y);
-
-    //Linear combination of vectors
-    std::cout << "x + 2y + 3z: " << (x + (2*y) + (3*z));
-
-    //DOT & NORM2
-    std::cout << "dot(x, y) = " << Dot(x, y) << std::endl;
-    std::cout << "length(x) = " << Norm2(x) << std::endl;
-    std::cout << "length(y) = " << Norm2(y) << std::endl;
-
-    LaffVector r(10);
-    r.Randomize(0, 50);
-    std::cout << "r: " << r;
-
-
-    //SLICE
-    LaffVector forward_slice = r.Slice(2, 5, 1);
-    LaffVector reverse_slice = r.Slice(8, 3, -2); //with stride -2
-
-    std::cout << "Slice(2, 5, 1): " << forward_slice;
-    std::cout << "Slice(8, 3, -2): " << reverse_slice;
-
-    return 0;
+    if (v._name.size() > 0)
+        output << v._name << ": ";
+    output << "( ";
+    for(auto it = v._lvector.begin(); it != v._lvector.end(); it++)
+        output << *it << " ";
+    output << " )\n";
+    return output;
 }
+
